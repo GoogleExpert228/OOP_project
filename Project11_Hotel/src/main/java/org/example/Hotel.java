ec6d1da1c9703d5836e5e77f6060b45eda46ea6e
GoogleExpert228 <Danil.engels28@gmail.com>
2024-04-09 23:58:25 +0300

    test commit

--- Project11_Hotel/src/main/java/org/example/Hotel.java
+++ Project11_Hotel/src/main/java/org/example/Hotel.java
@@ -10,6 +10,7 @@ import Guest.Guest;
 // then press Enter. You can now see whitespace characters in your code.
 public class Hotel {
     public static void main(String[] args) {
+        /*
         HotelRoom room101 = new HotelSingleRoom(101);
         Guest guest1 = new Guest(1, "John Doe", 30, "john@example.com", "+1234567890", "USA");
         RoomGuest reservation1 = new RoomGuest(room101, guest1);
@@ -19,5 +20,8 @@ public class Hotel {
         Guest guest2 = new Guest(2, "Alice Smith", 25, "alice@example.com", "+1987654321", "UK");
         RoomGuest reservation2 = new RoomGuest(room201, guest2);
         System.out.println(reservation2);
+
+         */
+        System.out.println("Hello world!");
         }
     }
